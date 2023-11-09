203b697159f6490a104ac085348b16ca352eaf7c
Jeffery Tang <jefery124@gmail.com>
2023-11-09 14:53:39 -0500

    improved readability, added input redirection

--- shell.c
+++ shell.c
@@ -1,63 +1,81 @@
-// Jeffery Tang 260791531
 
-/* Out OF ASSIGNMENT SCOPE
- * combination of or multiple redirection, piping, or & in a command
- * piping, redirection, background with built in commands (redirection may work)
- * && and || operators
- * treating multi-word strings as one parameter
- * running more than just a background task in a single line
- * return "command not found" if command doesn't exist (for me I just return a blank line)
- * producing more than 50 background tasks
+/* THINGS THIS SIMPLE SHELL CAN'T DO
+ * - combination of or multiple redirection, piping, or & in a command
+ * - piping, redirection, background with built in commands (redirection may
+ *   work)
+ * - && and || operators
+ * - treating multi-word strings as one parameter
+ * - running more than just a background task in a single line
+ * - return "command not found" if command doesn't exist (for me I just return a
+ *   blank line) producing more than 50 background tasks
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/wait.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int fg_pid = 0; // in parent, 0 means there's no foreground process, in child, it should always be 0
+const int MAX_NUM_BG_PROCESSES = 20;
+
+int fg_pid = 0;  // in parent, 0 means there's no foreground process
+                 // in child, it should always be 0
 int fg_pid2 = 0; // piping involves creating two processes
-char* line = NULL;
 
 int getcmd(
-    char* prompt, char* args[], int* background, int* redirect, int* pipe_index
+    char* prompt,
+    char* line,
+    char* args[],
+    bool* bg,
+    bool* redirect_out,
+    bool* redirect_in,
+    int* pipe_index
 ) {
     int length;
-    int i = 0;
+    int num_args = 0;
     char* token = NULL;
     char* loc = NULL;
     line = NULL;
-    size_t alloc_size = 0;
+    size_t alloc_size = 0; // unused
 
     printf("%s", prompt);
     length = getline(&line, &alloc_size, stdin);
+    // pressing enter registers \n as the last character of the line
     if (length <= 0) {
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     // Check if background is specified..
     loc = index(line, '&');
     if (loc != NULL) {
-        *background = 1;
+        *bg = true;
         *loc = ' '; // removes the &
     } else {
-        *background = 0;
+        *bg = false;
     }
 
-    // Check for output redirection >
+    // Check for redirection >, <
     loc = index(line, '>');
     if (loc != NULL) {
-        *redirect = 1;
+        *redirect_out = true;
         *loc = ' ';
     } else {
-        *redirect = 0;
+        *redirect_out = false;
+    }
+    loc = index(line, '<');
+    if (loc != NULL) {
+        *redirect_in = true;
+        *loc = ' ';
+    } else {
+        *redirect_in = false;
     }
 
-    char* line2 = line;
-    while ((token = strsep(&line2, " \t\n")) != NULL) {
+    char* to_be_parsed = line;
+    while ((token = strsep(&to_be_parsed, " \t\n")) != NULL) {
+        // deal with \r\n
         for (int j = 0; j < strlen(token); j++) {
             if (token[j] <= 32) {
                 token[j] = '\0';
@@ -66,216 +84,233 @@ int getcmd(
 
         if (strlen(token) > 0) {
             if (strcmp(token, "|") == 0) {
-                *pipe_index = i;
-                args[i] = NULL;
+                *pipe_index = num_args;
+                args[num_args] = NULL;
             } else {
-                args[i] = token;
+                args[num_args] = token;
             }
 
-            i++;
+            num_args++;
         }
     }
 
-    args[i] = NULL;
+    args[num_args] = NULL;
 
-    return i; // num arguments
+    return num_args;
 }
 
-void handle_sigstop(int sig) { // Ctrl-Z
-    // Ignore
-}
+// Ctrl-Z
+void handle_sigstop(int sig) {}
 
-void handle_sigint(int sig) { // Ctrl-C
-
+// Ctrl-C
+void handle_sigint(int sig) {
     if (fg_pid > 0) { // parent and there's an fg process
         kill(fg_pid, SIGKILL);
     }
-    if (fg_pid2 > 0) { // piping case
+    if (fg_pid2 > 0) { // for piping
         kill(fg_pid2, SIGKILL);
     }
 }
 
 int main(void) {
     // SIGNAL HANDLING
-    if (
-        signal(SIGTSTP, handle_sigstop) == SIG_ERR
-        || signal(SIGINT, handle_sigint) == SIG_ERR
-        ) {
+    if (signal(SIGTSTP, handle_sigstop) == SIG_ERR
+        || signal(SIGINT, handle_sigint) == SIG_ERR) {
         printf("ERROR! Could not bind the signal handler to parent. Exiting\n");
         exit(EXIT_FAILURE);
     }
 
-    int bg_pids[20];
-    char* bg_names[20]; // names of the processes
-    int nth_bg_pid = 0;
+    int bg_pids[MAX_NUM_BG_PROCESSES];
+    char* bg_process_names[MAX_NUM_BG_PROCESSES];
+    int num_bg_processes = 0;
 
     while (1) {
-        // PROMPT
+        char* line = NULL;
         char* args[50];
-        int bg = 0;
-        int redirect_out = 0;
-        int stdout = 0;
-        int fd = 0;
+        bool bg = false;
+        bool redirect_out = false;
+        bool redirect_in = false;
+        int my_stdout = 0;
+        int my_stdin = 0;
+        int fd_out = 0;
+        int fd_in = 0;
         int pipe_index = 0;
-        int num_args = getcmd("\nsh > ", args, &bg, &redirect_out, &pipe_index);
-
-        /* the steps can be..:
-        (1) fork a child process using fork()
-        (2) the child process will invoke execvp()
-        (3) if background is not specified, the parent will wait,
-        otherwise parent starts tarhe next command...
-        */
+        int num_args = getcmd(
+            "\nsh > ", line, args, &bg, &redirect_out, &redirect_in, &pipe_index
+        );
 
         if (args[0] == NULL) {
-            // onto the next command!
-        } else {
-            // REDIRECT OUT TO FILE
-            if (redirect_out) {
-                stdout = dup(STDOUT_FILENO);
-                close(STDOUT_FILENO);
-                fd = open(
-                    args[num_args - 1],
-                    O_WRONLY | O_CREAT | O_TRUNC, 0644
-                ); // O_TRUNC first erases the file
-                args[num_args - 1] = NULL;
-            }
+            goto free_line;
+        }
 
-            // BUILT-IN COMMANDS
-            if (strcmp(args[0], "cd") == 0) {
-                chdir(args[1]);
-            } else if (strcmp(args[0], "pwd") == 0) {
-                char* cwd = getcwd(NULL, 0);
-                printf("%s\n", cwd);
-                free(cwd);
-            } else if (strcmp(args[0], "exit") == 0) {
-                // exit out of background processes first
-                for (int i = 0; i < nth_bg_pid; i++) {
-                    kill(bg_pids[i], SIGKILL);
-                }
-                exit(0); // bye bye!
-            }
-            // single parameter for the index of background processes
-            // index starts from 1
-            // includes running and done bg processes
-            else if (strcmp(args[0], "fg") == 0) {
-                int nth_job = nth_bg_pid; // default argument, nth_job index starts from one
-                if (args[1] == NULL) {
-                    if (nth_job == 0) {
-                        nth_job = 1;
-                    } else {
-                        for (int i = nth_bg_pid; i > 0; i--) { // i is ith_job
-                            if (waitpid(bg_pids[i - 1], NULL, WNOHANG) == 0) { // still running
-                                nth_job = i;
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    nth_job = atoi(args[1]);
-                }
+        // OUTPUT REDIRECTION
+        if (redirect_out) {
+            my_stdout = dup(STDOUT_FILENO);
+            close(STDOUT_FILENO);
+            fd_out = open(
+                args[num_args - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644
+            ); // O_TRUNC overwrites the file
+            args[num_args - 1] = NULL;
+        }
+        // INPUT REDIRECTION
+        if (redirect_in) {
+            my_stdin = dup(STDIN_FILENO);
+            close(STDIN_FILENO);
+            fd_in = open(args[num_args - 1], O_RDONLY, 0644);
+            args[num_args - 1] = NULL;
+        }
 
-                if (nth_job == 0) {
-                    printf("Invalid argument");
-                } else if (nth_bg_pid == 0) {
+        // BUILT-IN COMMANDS
+        if (strcmp(args[0], "cd") == 0) {
+            chdir(args[1]);
+        } else if (strcmp(args[0], "pwd") == 0) {
+            char* cwd = getcwd(NULL, 0);
+            printf("%s\n", cwd);
+            free(cwd);
+        } else if (strcmp(args[0], "exit") == 0) {
+            // exit out of background processes first
+            for (int i = 0; i < num_bg_processes; i++) {
+                kill(getpid(), SIGKILL);
+            }
+            exit(EXIT_SUCCESS); // bye bye!
+        } else if (strcmp(args[0], "fg") == 0) {
+            // - optional parameter: job index
+            //   - default: index of the most recently created background
+            //   process
+            //   - index starts from 1
+            // - finished bg processes keep their index
+
+            int nth_job;
+            if (args[1] == NULL) {
+                if (num_bg_processes == 0) {
                     printf("No background jobs yet");
-                } else if (nth_job > nth_bg_pid) {
-                    printf("Job number is only up to %d", nth_bg_pid);
-                } else {
-                    fg_pid = bg_pids[nth_job - 1];
-                    waitpid(bg_pids[nth_job - 1], NULL, 0);
+                    goto free_line;
                 }
-            } else if (strcmp(args[0], "jobs") == 0) {
-                printf("Running - index\tpid\tname\n");
-                for (int i = 0; i < nth_bg_pid; i++) {
-                    if (waitpid(bg_pids[i], NULL, WNOHANG) == 0) { // still running
-                        printf(
-                            "\t  %i\t%i\t%s\n", i + 1,
-                            bg_pids[i],
-                            bg_names[i]
-                        );
+
+                // Some background processes are done. We want to go to the
+                // first process that is still running.
+                for (int i = num_bg_processes; i > 0; i--) {
+                    if (waitpid(bg_pids[i - 1], NULL, WNOHANG) == 0) {
+                        nth_job = i;
+                        break;
+                    }
+
+                    if (i == 1) { // all the background processes are done
+                        printf("No running background jobs yet");
+                        goto free_line;
                     }
                 }
+            } else {
+                nth_job = atoi(args[1]);
             }
-            // PIPE/PIPING
-            else if (pipe_index != 0) {
-                int p[2];
-                if (pipe(p) == -1) {
-                    printf("Pipe error, exiting program\n");
-                    exit(EXIT_FAILURE);
-                }
 
-                fg_pid = fork();
-                if (fg_pid == 0) {
-                    dup2(p[1], STDOUT_FILENO);
-                    close(p[0]);
-                    close(p[1]);
-                    execvp(args[0], args);
-                } else if (fg_pid < 0) {
+            if (nth_job <= 0) {
+                printf("Invalid argument");
+            } else if (nth_job > num_bg_processes) {
+                printf("Job number is only up to %d", num_bg_processes);
+            } else {
+                fg_pid = bg_pids[nth_job - 1];
+                waitpid(bg_pids[nth_job - 1], NULL, 0);
+            }
+        } else if (strcmp(args[0], "jobs") == 0) {
+            printf("Running - index\tpid\tname\n");
+            for (int i = 0; i < num_bg_processes; i++) {
+                if (waitpid(bg_pids[i], NULL, WNOHANG) == 0) { // still running
                     printf(
-                        "Failed to create child process for piping! Exiting now.\n"
+                        "\t  %i\t%i\t%s\n",
+                        i + 1,
+                        bg_pids[i],
+                        bg_process_names[i]
                     );
-                    exit(EXIT_FAILURE);
                 }
+            }
+        }
+        // PIPING |
+        else if (pipe_index != 0) {
+            int p[2];
+            if (pipe(p) == -1) {
+                printf("Pipe error, exiting program\n");
+                exit(EXIT_FAILURE);
+            }
 
-                fg_pid2 = fork();
-                if (fg_pid2 == 0) {
-                    dup2(p[0], STDIN_FILENO);
-                    close(p[0]);
-                    close(p[1]);
-                    execvp(*(args + pipe_index + 1), args + pipe_index + 1);
-                } else if (fg_pid2 < 0) {
-                    printf(
-                        "Failed to create child process for piping! Exiting now.\n"
-                    );
-                    exit(EXIT_FAILURE);
-                }
-
+            fg_pid = fork();
+            if (fg_pid == 0) {
+                dup2(p[1], STDOUT_FILENO);
                 close(p[0]);
                 close(p[1]);
-
-                waitpid(fg_pid, NULL, 0);
-                waitpid(fg_pid2, NULL, 0);
+                execvp(args[0], args);
+            } else if (fg_pid < 0) {
+                printf("Failed to create child process for piping! Exiting "
+                       "now.\n");
+                exit(EXIT_FAILURE);
             }
-            // NON-PIPING EXTERNAL COMMANDS
-            else {
 
-                fg_pid = fork();
+            fg_pid2 = fork();
+            if (fg_pid2 == 0) {
+                dup2(p[0], STDIN_FILENO);
+                close(p[0]);
+                close(p[1]);
+                execvp(*(args + pipe_index + 1), args + pipe_index + 1);
+            } else if (fg_pid2 < 0) {
+                printf("Failed to create child process for piping! Exiting "
+                       "now.\n");
+                exit(EXIT_FAILURE);
+            }
 
-                if (fg_pid < 0) {
-                    printf("Failed to create child process! Exiting now.\n");
-                    exit(EXIT_FAILURE);
-                } else if (fg_pid == 0) { // child
-                    if (bg == 1) { // if i don't include this Ctrl-C will cancel all the processes
-                        signal(SIGINT, SIG_IGN);
-                    }
-                    signal(SIGTSTP, SIG_IGN);
+            close(p[0]);
+            close(p[1]);
 
-                    if (execvp(args[0], args) == -1) {
-                        printf("Error executing: %s\n", args[0]);
-                        exit(EXIT_FAILURE);
-                    }
-                } else { // parent
-                    if (bg == 0) { // if it's a foreground task
-                        waitpid(fg_pid, NULL, 0);
-                    } else {
-                        bg_pids[nth_bg_pid] = fg_pid;
-                        bg_names[nth_bg_pid] = args[0];
-                        nth_bg_pid++;
-                    }
+            waitpid(fg_pid, NULL, 0);
+            waitpid(fg_pid2, NULL, 0);
+        }
+        // NON-PIPING EXTERNAL COMMANDS
+        // Fork a child and have the child run the command, if & is in command,
+        // immediately prompt for next command. If
+        else {
+            fg_pid = fork();
+
+            if (fg_pid < 0) {
+                printf("Failed to create child process! Exiting now.\n");
+                exit(EXIT_FAILURE);
+            } else if (fg_pid == 0) {
+                if (bg) {
+                    // if i don't include this Ctrl-C will cancel all the
+                    // processes
+                    signal(SIGINT, SIG_IGN);
                 }
-            }
+                signal(SIGTSTP, SIG_IGN);
 
-            fg_pid = 0;
-            fg_pid2 = 0;
-
-            // UNDO FILE REDIRECTION
-            if (redirect_out == 1) {
-                close(fd);
-                dup(stdout);
-                close(stdout);
+                if (execvp(args[0], args) == -1) {
+                    printf("Error executing: %s\n", args[0]);
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                if (!bg) {
+                    waitpid(fg_pid, NULL, 0);
+                } else {
+                    bg_pids[num_bg_processes] = fg_pid;
+                    bg_process_names[num_bg_processes] = args[0];
+                    num_bg_processes++;
+                }
             }
         }
 
+        fg_pid = 0;
+        fg_pid2 = 0;
+
+        // UNDO REDIRECTION
+        if (redirect_out) {
+            close(fd_out);
+            dup(my_stdout);
+            close(my_stdout);
+        }
+        if (redirect_in) {
+            close(fd_in);
+            dup(my_stdin);
+            close(my_stdin);
+        }
+
+    free_line:
         if (line != NULL) {
             free(line);
         }
